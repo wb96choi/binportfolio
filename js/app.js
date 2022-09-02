7d002c01a435c5c5d07f2c2eebf1c31aa67d166e
1_b1n <choi44601280@gmail.com>
2022-09-02 12:29:14 +0900

    확인

--- js/app.js
+++ js/app.js
@@ -118,182 +118,181 @@ $(function () {
 
 // 프로그레스바 크기 반응형
 $(function () {
+    $(window).resize(function () {
+        if (window.matchMedia("(max-width: 375px)").matches) {
+            $(window).scroll(function () {
+                let scrollposition = $(window).scrollTop();
+                // about second floor 프로그레스바 
+                let Skillsection = document.querySelector(".second-floor").offsetTop;
 
-    if (window.matchMedia("(max-width: 375px)").matches) {
-        $(window).scroll(function () {
-            let scrollposition = $(window).scrollTop();
-            // about second floor 프로그레스바 
-            let Skillsection = document.querySelector(".second-floor").offsetTop;
+                function showProgress() {
+                    // html
+                    $('.prog-html').circleProgress({
+                        value: 0.9,
+                        startAngle: -Math.PI / 2,
+                        size: 30,
+                        thickness: 5,
+                        fill: {
+                            color: '#ffc0cb'
+                        }
+                    });
+                    // css
+                    $('.prog-css').circleProgress({
+                        value: 0.85,
+                        startAngle: -Math.PI / 2,
+                        size: 30,
+                        thickness: 5,
+                        fill: {
+                            color: '#ffc0cb'
+                        }
+                    });
+                    // javascript
+                    $('.prog-js').circleProgress({
+                        value: 0.85,
+                        startAngle: -Math.PI / 2,
+                        size: 30,
+                        thickness: 5,
+                        fill: {
+                            color: '#ffc0cb'
+                        }
+                    });
+                    // jquery
+                    $('.prog-jq').circleProgress({
+                        value: 0.85,
+                        startAngle: -Math.PI / 2,
+                        size: 30,
+                        thickness: 5,
+                        fill: {
+                            color: '#ffc0cb'
+                        }
+                    });
+                    // Figma
+                    $('.prog-figma').circleProgress({
+                        value: 0.7,
+                        startAngle: -Math.PI / 2,
+                        size: 30,
+                        thickness: 5,
+                        fill: {
+                            color: '#ffc0cb'
+                        }
+                    });
+                    // photoshop
+                    $('.prog-ps').circleProgress({
+                        value: 0.9,
+                        startAngle: -Math.PI / 2,
+                        size: 30,
+                        thickness: 5,
+                        fill: {
+                            color: '#ffc0cb'
+                        }
+                    });
+                    // Figma
+                    $('.prog-illust').circleProgress({
+                        value: 0.75,
+                        startAngle: -Math.PI / 2,
+                        size: 30,
+                        thickness: 5,
+                        fill: {
+                            color: '#ffc0cb'
+                        }
+                    });
 
-            function showProgress() {
-                // html
-                $('.prog-html').circleProgress({
-                    value: 0.9,
-                    startAngle: -Math.PI / 2,
-                    size: 30,
-                    thickness: 5,
-                    fill: {
-                        color: '#ffc0cb'
-                    }
-                });
-                // css
-                $('.prog-css').circleProgress({
-                    value: 0.85,
-                    startAngle: -Math.PI / 2,
-                    size: 30,
-                    thickness: 5,
-                    fill: {
-                        color: '#ffc0cb'
-                    }
-                });
-                // javascript
-                $('.prog-js').circleProgress({
-                    value: 0.85,
-                    startAngle: -Math.PI / 2,
-                    size: 30,
-                    thickness: 5,
-                    fill: {
-                        color: '#ffc0cb'
-                    }
-                });
-                // jquery
-                $('.prog-jq').circleProgress({
-                    value: 0.85,
-                    startAngle: -Math.PI / 2,
-                    size: 30,
-                    thickness: 5,
-                    fill: {
-                        color: '#ffc0cb'
-                    }
-                });
-                // Figma
-                $('.prog-figma').circleProgress({
-                    value: 0.7,
-                    startAngle: -Math.PI / 2,
-                    size: 30,
-                    thickness: 5,
-                    fill: {
-                        color: '#ffc0cb'
-                    }
-                });
-                // photoshop
-                $('.prog-ps').circleProgress({
-                    value: 0.9,
-                    startAngle: -Math.PI / 2,
-                    size: 30,
-                    thickness: 5,
-                    fill: {
-                        color: '#ffc0cb'
-                    }
-                });
-                // Figma
-                $('.prog-illust').circleProgress({
-                    value: 0.75,
-                    startAngle: -Math.PI / 2,
-                    size: 30,
-                    thickness: 5,
-                    fill: {
-                        color: '#ffc0cb'
-                    }
-                });
+                };
 
-            };
+                if (scrollposition <= Skillsection) {
+                    showProgress();
+                }
+            });
+        } else {
+            $(window).scroll(function () {
+                let scrollposition = $(window).scrollTop();
+                // about second floor 프로그레스바 
+                let Skillsection = document.querySelector(".second-floor").offsetTop;
 
-            if (scrollposition <= Skillsection) {
-                showProgress();
-            }
-        });
-    } else {
-        $(window).scroll(function () {
-            let scrollposition = $(window).scrollTop();
-            // about second floor 프로그레스바 
-            let Skillsection = document.querySelector(".second-floor").offsetTop;
+                function showProgress() {
+                    // html
+                    $('.prog-html').circleProgress({
+                        value: 0.9,
+                        startAngle: -Math.PI / 2,
+                        size: 50,
+                        thickness: 5,
+                        fill: {
+                            color: '#ffc0cb'
+                        }
+                    });
+                    // css
+                    $('.prog-css').circleProgress({
+                        value: 0.85,
+                        startAngle: -Math.PI / 2,
+                        size: 50,
+                        thickness: 5,
+                        fill: {
+                            color: '#ffc0cb'
+                        }
+                    });
+                    // javascript
+                    $('.prog-js').circleProgress({
+                        value: 0.85,
+                        startAngle: -Math.PI / 2,
+                        size: 50,
+                        thickness: 5,
+                        fill: {
+                            color: '#ffc0cb'
+                        }
+                    });
+                    // jquery
+                    $('.prog-jq').circleProgress({
+                        value: 0.85,
+                        startAngle: -Math.PI / 2,
+                        size: 50,
+                        thickness: 5,
+                        fill: {
+                            color: '#ffc0cb'
+                        }
+                    });
+                    // Figma
+                    $('.prog-figma').circleProgress({
+                        value: 0.7,
+                        startAngle: -Math.PI / 2,
+                        size: 50,
+                        thickness: 5,
+                        fill: {
+                            color: '#ffc0cb'
+                        }
+                    });
+                    // photoshop
+                    $('.prog-ps').circleProgress({
+                        value: 0.9,
+                        startAngle: -Math.PI / 2,
+                        size: 50,
+                        thickness: 5,
+                        fill: {
+                            color: '#ffc0cb'
+                        }
+                    });
+                    // Figma
+                    $('.prog-illust').circleProgress({
+                        value: 0.75,
+                        startAngle: -Math.PI / 2,
+                        size: 50,
+                        thickness: 5,
+                        fill: {
+                            color: '#ffc0cb'
+                        }
+                    });
 
-            function showProgress() {
-                // html
-                $('.prog-html').circleProgress({
-                    value: 0.9,
-                    startAngle: -Math.PI / 2,
-                    size: 50,
-                    thickness: 5,
-                    fill: {
-                        color: '#ffc0cb'
-                    }
-                });
-                // css
-                $('.prog-css').circleProgress({
-                    value: 0.85,
-                    startAngle: -Math.PI / 2,
-                    size: 50,
-                    thickness: 5,
-                    fill: {
-                        color: '#ffc0cb'
-                    }
-                });
-                // javascript
-                $('.prog-js').circleProgress({
-                    value: 0.85,
-                    startAngle: -Math.PI / 2,
-                    size: 50,
-                    thickness: 5,
-                    fill: {
-                        color: '#ffc0cb'
-                    }
-                });
-                // jquery
-                $('.prog-jq').circleProgress({
-                    value: 0.85,
-                    startAngle: -Math.PI / 2,
-                    size: 50,
-                    thickness: 5,
-                    fill: {
-                        color: '#ffc0cb'
-                    }
-                });
-                // Figma
-                $('.prog-figma').circleProgress({
-                    value: 0.7,
-                    startAngle: -Math.PI / 2,
-                    size: 50,
-                    thickness: 5,
-                    fill: {
-                        color: '#ffc0cb'
-                    }
-                });
-                // photoshop
-                $('.prog-ps').circleProgress({
-                    value: 0.9,
-                    startAngle: -Math.PI / 2,
-                    size: 50,
-                    thickness: 5,
-                    fill: {
-                        color: '#ffc0cb'
-                    }
-                });
-                // Figma
-                $('.prog-illust').circleProgress({
-                    value: 0.75,
-                    startAngle: -Math.PI / 2,
-                    size: 50,
-                    thickness: 5,
-                    fill: {
-                        color: '#ffc0cb'
-                    }
-                });
+                };
 
-            };
-
-            if (scrollposition <= Skillsection) {
-                showProgress();
-            }
-        });
-    }
-
+                if (scrollposition <= Skillsection) {
+                    showProgress();
+                }
+            });
+        };
+    });
 });
 
 // 모바일 토글메뉴
 $(function () {
-
     $('#toggle-menu-btn').click(function () {
         $('#toggle-menu-btn').toggleClass('active');
         $('#mobile-toggle-menu').toggleClass('on');
