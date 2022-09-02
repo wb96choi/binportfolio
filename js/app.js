9c4d35ede85a0b986a187dcfeed330b1dd6f1da1
1_b1n <choi44601280@gmail.com>
2022-09-02 14:50:35 +0900

    resize delete

--- js/app.js
+++ js/app.js
@@ -118,8 +118,9 @@ $(function () {
 
 // 프로그레스바 크기 반응형
 $(function () {
-    $(window).resize(function () {
-        if (window.matchMedia("(max-width: 375px)").matches) {
+
+    if (window.matchMedia("(max-width: 375px)").matches) {
+        
             $(window).scroll(function () {
                 let scrollposition = $(window).scrollTop();
                 // about second floor 프로그레스바 
@@ -203,92 +204,93 @@ $(function () {
                     showProgress();
                 }
             });
-        } else {
-            $(window).scroll(function () {
-                let scrollposition = $(window).scrollTop();
-                // about second floor 프로그레스바 
-                let Skillsection = document.querySelector(".second-floor").offsetTop;
+        
+    } else {
+        $(window).scroll(function () {
+            let scrollposition = $(window).scrollTop();
+            // about second floor 프로그레스바 
+            let Skillsection = document.querySelector(".second-floor").offsetTop;
 
-                function showProgress() {
-                    // html
-                    $('.prog-html').circleProgress({
-                        value: 0.9,
-                        startAngle: -Math.PI / 2,
-                        size: 50,
-                        thickness: 5,
-                        fill: {
-                            color: '#ffc0cb'
-                        }
-                    });
-                    // css
-                    $('.prog-css').circleProgress({
-                        value: 0.85,
-                        startAngle: -Math.PI / 2,
-                        size: 50,
-                        thickness: 5,
-                        fill: {
-                            color: '#ffc0cb'
-                        }
-                    });
-                    // javascript
-                    $('.prog-js').circleProgress({
-                        value: 0.85,
-                        startAngle: -Math.PI / 2,
-                        size: 50,
-                        thickness: 5,
-                        fill: {
-                            color: '#ffc0cb'
-                        }
-                    });
-                    // jquery
-                    $('.prog-jq').circleProgress({
-                        value: 0.85,
-                        startAngle: -Math.PI / 2,
-                        size: 50,
-                        thickness: 5,
-                        fill: {
-                            color: '#ffc0cb'
-                        }
-                    });
-                    // Figma
-                    $('.prog-figma').circleProgress({
-                        value: 0.7,
-                        startAngle: -Math.PI / 2,
-                        size: 50,
-                        thickness: 5,
-                        fill: {
-                            color: '#ffc0cb'
-                        }
-                    });
-                    // photoshop
-                    $('.prog-ps').circleProgress({
-                        value: 0.9,
-                        startAngle: -Math.PI / 2,
-                        size: 50,
-                        thickness: 5,
-                        fill: {
-                            color: '#ffc0cb'
-                        }
-                    });
-                    // Figma
-                    $('.prog-illust').circleProgress({
-                        value: 0.75,
-                        startAngle: -Math.PI / 2,
-                        size: 50,
-                        thickness: 5,
-                        fill: {
-                            color: '#ffc0cb'
-                        }
-                    });
+            function showProgress() {
+                // html
+                $('.prog-html').circleProgress({
+                    value: 0.9,
+                    startAngle: -Math.PI / 2,
+                    size: 50,
+                    thickness: 5,
+                    fill: {
+                        color: '#ffc0cb'
+                    }
+                });
+                // css
+                $('.prog-css').circleProgress({
+                    value: 0.85,
+                    startAngle: -Math.PI / 2,
+                    size: 50,
+                    thickness: 5,
+                    fill: {
+                        color: '#ffc0cb'
+                    }
+                });
+                // javascript
+                $('.prog-js').circleProgress({
+                    value: 0.85,
+                    startAngle: -Math.PI / 2,
+                    size: 50,
+                    thickness: 5,
+                    fill: {
+                        color: '#ffc0cb'
+                    }
+                });
+                // jquery
+                $('.prog-jq').circleProgress({
+                    value: 0.85,
+                    startAngle: -Math.PI / 2,
+                    size: 50,
+                    thickness: 5,
+                    fill: {
+                        color: '#ffc0cb'
+                    }
+                });
+                // Figma
+                $('.prog-figma').circleProgress({
+                    value: 0.7,
+                    startAngle: -Math.PI / 2,
+                    size: 50,
+                    thickness: 5,
+                    fill: {
+                        color: '#ffc0cb'
+                    }
+                });
+                // photoshop
+                $('.prog-ps').circleProgress({
+                    value: 0.9,
+                    startAngle: -Math.PI / 2,
+                    size: 50,
+                    thickness: 5,
+                    fill: {
+                        color: '#ffc0cb'
+                    }
+                });
+                // Figma
+                $('.prog-illust').circleProgress({
+                    value: 0.75,
+                    startAngle: -Math.PI / 2,
+                    size: 50,
+                    thickness: 5,
+                    fill: {
+                        color: '#ffc0cb'
+                    }
+                });
 
-                };
+            };
 
-                if (scrollposition <= Skillsection) {
-                    showProgress();
-                }
-            });
-        };
-    });
+            if (scrollposition <= Skillsection) {
+                showProgress();
+            }
+        });
+    };
+
 });
 
 // 모바일 토글메뉴
@@ -421,6 +423,7 @@ $(function () {
         $(this).find('.text-hidden').stop().fadeOut(500);
     });
 });
+
 
 // Contact 애니메이션
 $(function () {
